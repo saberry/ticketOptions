b8c1381766ce03fd68e750bd652f0c4adece393e
Seth Berry <seth.berry@nd.edu>
2018-09-14 16:14:30 -0400

    historical prices

---
+++ code/historical.R
@@ -0,0 +1,42 @@
+#####################################
+### Historical JSON From CFP-RSVP ###
+#####################################
+
+performerID = readr::read_csv("data/completeData.csv")
+
+ids = performerID$id
+
+rm(performerID)
+
+allHistory = lapply(ids, function(x) {
+  
+  Sys.sleep(.25)
+  
+  pageID = paste("https://api.dibitnow.com/api/v1/eventPerformers/getEventPerformer?eventPerformerId=", 
+                 x, sep = "") 
+  
+  out = jsonlite::read_json(pageID, simplifyVector = TRUE)
+  
+  lowerLevelChange = out$priceTrend$lowerLevelPriceChanges
+  
+  names(lowerLevelChange) = c("lowerLevelPrice", "dateTicks")
+  
+  lowerLevelChange$name = out$name
+  
+  lowerLevelChange = reshape2::melt(lowerLevelChange, 
+                                    id.vars = c("name", "dateTicks"))
+  
+  upperLevelChange = out$priceTrend$upperLevelPriceChanges
+  
+  names(upperLevelChange) = c("upperLevelPrice", "dateTicks")
+  
+  upperLevelChange$name = out$name
+  
+  upperLevelChange = reshape2::melt(upperLevelChange, 
+                                    id.vars = c("name", "dateTicks"))
+  
+  out = bind_rows(lowerLevelChange, upperLevelChange)
+  
+  return(out)
+})
+
